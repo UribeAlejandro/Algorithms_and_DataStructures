5368666834ace24d672e59b002536c7994d6f963
Alejandro Uribe <jauriberamirez@gmail.com>
2023-04-07 11:21:41 -0300

    Queue: from linked list working, from array not all methods working

---
+++ src/main/java/data_structures/queues/QueueFromLinkedList.java
@@ -0,0 +1,45 @@
+package data_structures.queues;
+
+import data_structures.utils.ListNode;
+
+public class QueueFromLinkedList {
+
+  private int size;
+  private ListNode first;
+  private ListNode last;
+
+  public QueueFromLinkedList() {
+    size = 0;
+    first = null;
+  }
+
+  public void enqueue(int value) {
+    size++;
+    ListNode oldLast = last;
+    last = new ListNode(value, null);
+    if (isEmpty()) {
+      first = last;
+    } else {
+      oldLast.next = last;
+    }
+  }
+
+  public int dequeue() throws NullPointerException {
+    int value = first.val;
+    first = first.next;
+    size--;
+
+    if (isEmpty()) {
+      throw new NullPointerException();
+    }
+    return value;
+  }
+
+  public boolean isEmpty() {
+    return first == null;
+  }
+
+  public int size() {
+    return size;
+  }
+}
