58df6647c32952be3116d1c46c64d82be2e8e43d
Alejandro Uribe <jauriberamirez@gmail.com>
2023-04-10 22:48:51 -0300

    Stack with generics

---
+++ src/main/java/data_structures/stacks/StackGenerics.java
@@ -0,0 +1,32 @@
+package data_structures.stacks;
+
+import data_structures.utils.ListNodeGeneric;
+import java.util.EmptyStackException;
+
+public class StackGenerics<Item> {
+
+  private int size;
+  private Item top;
+  private ListNodeGeneric<Item> stack;
+
+  public StackGenerics() {
+    size = -1;
+    stack = null;
+  }
+
+  public void push(Item value) {
+    size++;
+    stack = new ListNodeGeneric<>(value, stack);
+  }
+
+  public Item pop() throws EmptyStackException {
+    if (stack == null) {
+      throw new EmptyStackException();
+    } else {
+      size--;
+      top = stack.val;
+      stack = stack.next;
+      return top;
+    }
+  }
+}
---
+++ src/test/java/data_structures/stacks/StackGenericsTest.java
@@ -0,0 +1,33 @@
+package data_structures.stacks;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class StackGenericsTest {
+
+  private StackGenerics<String> stackString;
+  private StackGenerics<Integer> stackInteger;
+
+  @BeforeEach
+  void setUp() {
+    stackString = new StackGenerics<>();
+    stackInteger = new StackGenerics<>();
+  }
+
+  @Test
+  void push() {
+    String expectedS = "Hello";
+    stackString.push(expectedS);
+    assertEquals(expectedS, stackString.pop());
+
+    Integer expectedI = Integer.valueOf(1);
+    stackInteger.push(expectedI);
+    assertEquals(expectedI, stackInteger.pop());
+  }
+
+  @Test
+  void pop() {
+  }
+}
